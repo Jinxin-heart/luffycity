406564bb6b0a206bd49280333ffcfe525d850711
jinx <jinxin_heart@qq.com>
2026-08-21 23:56:12 +0800

    feature:测试django子应用，配置home路由

---
+++ luffycityapi/luffycityapi/apps/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HomeAPIView(APIView):
+    def get(self, request):
+        print("hello")
+        brother = ['jinx', 'jin', 'jinxin']
+        return Response(brother, status.HTTP_200_OK)
