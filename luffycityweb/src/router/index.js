aa9fb17f4035f4ccc447b3920e4b84e27e915a83
jinx <jinxin_heart@qq.com>
2026-08-22 17:53:19 +0800

    feature:创建前端首页和登陆的组件

--- luffycityweb/src/router/index.js
+++ luffycityweb/src/router/index.js
@@ -1,7 +1,24 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
 const routes = [
-
+    {
+        meta: {
+            title: "luffy2.0-站点首页",
+            keepAlive: true
+        },
+        path: '/',         // uri访问地址
+        name: "Home",
+        component: () => import("../views/Home.vue")
+    },
+    {
+        meta: {
+            title: "luffy2.0-用户登录",
+            keepAlive: true
+        },
+        path: '/login',      // uri访问地址
+        name: "Login",
+        component: () => import("../views/Login.vue")
+    }
 ]
 
 const router = createRouter(
