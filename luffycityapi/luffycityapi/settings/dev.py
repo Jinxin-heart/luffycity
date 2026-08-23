df3638420899b7cc8fb4005cc96f72d35b6c3aff
jinx <jinxin_heart@qq.com>
2026-08-23 14:17:05 +0800

    feature:后端使用的CORS，前后端分离项目，前端与后端使用一种就行。服务端和客户端分配不同域名，仿真开发，CORS跨域配置

--- luffycityapi/luffycityapi/settings/dev.py
+++ luffycityapi/luffycityapi/settings/dev.py
@@ -40,11 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
     'rest_framework',
     'home',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'luffycityapi.urls'
 
