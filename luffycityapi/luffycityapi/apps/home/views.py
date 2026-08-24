e8440164329a6eef90fb32eeeaf3de94ed0a171c
jinx <jinxin_heart@qq.com>
2026-08-24 21:16:43 +0800

    feature:feature:api服务端实现轮播广告接口

--- luffycityapi/luffycityapi/apps/home/views.py
+++ luffycityapi/luffycityapi/apps/home/views.py
@@ -1,7 +1,7 @@
 import constants
 from rest_framework.generics import ListAPIView
-from .models import Nav
-from .serializers import NavModelSerializer
+from .models import Nav, Banner
+from .serializers import NavModelSerializer, BannerModelSerializer
 
 
 
@@ -15,3 +15,9 @@ class NavFooterListAPIView(ListAPIView):
     """脚部导航视图"""
     queryset = Nav.objects.filter(position=constants.NAV_FOOTER_POSITION, is_show=True, is_deleted=False).order_by("orders", "-id")[:constants.NAV_FOOTER_SIZE]
     serializer_class = NavModelSerializer
+
+
+class BannerListAPIView(ListAPIView):
+    """轮播广告视图"""
+    queryset = Banner.objects.filter(is_show=True, is_deleted=False).order_by("orders", "-id")[:constants.BANNER_SIZE]
+    serializer_class = BannerModelSerializer
