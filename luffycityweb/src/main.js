6ba3a360bb68a611396be69c48f5c7992ab32fb5
jinx <164964990+Jinxin-heart@users.noreply.github.com>
2026-08-28 21:47:42 +0800

    feature:客户端集成腾讯云验证码

--- luffycityweb/src/main.js
+++ luffycityweb/src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'element-plus/dist/index.css';
 import store from './store'
+
 import router from './router/index.js'
 
 createApp(App).use(router).use(store).mount('#app')
