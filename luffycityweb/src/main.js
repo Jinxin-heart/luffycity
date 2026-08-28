a3462a6eca92e87285f1589fdd40e54645300fcf
jinx <164964990+Jinxin-heart@users.noreply.github.com>
2026-08-28 20:00:29 +0800

    feature:客户端基于vuex存储本地全局数据并判断登陆状态

--- luffycityweb/src/main.js
+++ luffycityweb/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 import 'element-plus/dist/index.css';
+import store from './store'
 import router from './router/index.js'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
---
+++ luffycityweb/src/views/User.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "User"
+}
+</script>
+
+<template>
+用户中心
+</template>
+
+<style scoped>
+
+</style>
