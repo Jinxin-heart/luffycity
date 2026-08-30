82e4cb3fb4d8eb0a6000baa24e5802de2002ae4c
jinx <164964990+Jinxin-heart@users.noreply.github.com>
2026-08-30 19:19:23 +0800

    feature:注册功能实现流程-客户端请求发送短信并实现短信倒计时冷却提示!

--- luffycityweb/src/api/user.js
+++ luffycityweb/src/api/user.js
@@ -1,4 +1,4 @@
-import http from '../utils/http.js'
+import http from "../utils/http"
 import {reactive, ref} from "vue"
 
 const user = reactive({
@@ -8,6 +8,10 @@ const user = reactive({
     remember: false, // 是否记住登录状态
     re_password: "",// 确认密码
     code: "", // 短信验证码
+    sms_btn_text: "点击获取验证码", // 短信按钮提示
+    is_send: false,  // 短信发送的标记
+    sms_interval: 60,// 间隔时间
+    interval: null,  // 定时器的标记
     login(res){
         // 用户登录
         return http.post("/users/login/", {
@@ -23,11 +27,14 @@ const user = reactive({
     },
     register(data){
         data.mobile = this.mobile
-        data.re_password = this.re_password
+        data.re_password = this.mobile
         data.password = this.password
         data.sms_code = this.code
         // 用户注册请求
         return http.post("/users/register/", data)
+    },
+    get_sms_code(){
+        return http.get(`/users/sms/${this.mobile}/`)
     }
 })
 
