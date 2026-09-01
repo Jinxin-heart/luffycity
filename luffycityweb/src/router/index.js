c80029055f4d46ab58a96cf283890b575334c04e
jinx <164964990+Jinxin-heart@users.noreply.github.com>
2026-09-01 22:06:39 +0800

    feature: 展示课程列表页

--- luffycityweb/src/router/index.js
+++ luffycityweb/src/router/index.js
@@ -39,6 +39,15 @@ const routes = [
     name: "User",
     component: ()=> import("../views/User.vue"),
   },
+  {
+    meta:{
+        title: "luffy2.0-课程列表",
+        keepAlive: true,
+    },
+    path: '/project',
+    name: "Course",
+    component: ()=> import("../views/Course.vue"),
+  },
 ]
 
 // 路由对象实例化
