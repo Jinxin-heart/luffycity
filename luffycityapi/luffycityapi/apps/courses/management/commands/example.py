3d5541b445db70e50dac458e9f8ebf5c104d2ba1
jinx <164964990+Jinxin-heart@users.noreply.github.com>
2026-09-07 12:11:09 +0800

    feature: Faker 添加模拟数据

--- luffycityapi/luffycityapi/apps/courses/management/commands/example.py
+++ luffycityapi/luffycityapi/apps/courses/management/commands/example.py
@@ -1,4 +1,5 @@
-import constants
+import constants, random
+from faker import Faker
 from django.core.management.base import BaseCommand, CommandError
 from courses.models import Teacher
 
@@ -8,7 +9,25 @@ class Command(BaseCommand):
 
     # 如果当前命令，需要接受来自终端的参数，可以使用add_arguments
     def add_arguments(self, parser):
-        pass
+        # 位置参数，必填项
+        # parser.add_argument('date_type', nargs='+', type=int, help="添加数据的类型")
+
+        # 命令参数，可选项
+        parser.add_argument(
+            '--type',
+            dest='type',
+            default='teacher',
+            type=str,
+            help='测试数据的类型',
+        )
+
+        parser.add_argument(
+            '--number',
+            dest='number',
+            default=10,
+            type=int,
+            help='添加数据的数量',
+        )
         # 位置参数，必填项
         # parser.add_argument('name', nargs='+', type=int)
 
@@ -21,14 +40,38 @@ class Command(BaseCommand):
 
     # 命令执行的核心方法，
     def handle(self, *args, **options):
-        """添加测试数据"""
-        print("添加测试数据")
+        """添加课程相关的测试数据"""
+        if options["type"] == "teacher":
+            self.add_teacher(options)
+        elif options["type"] == "direction":
+            self.add_direction(options)
 
-        Teacher.objects.create(
-            name="赵小明",
-            avatar="teacher/avatar.jpg",
-            role=1,
-            title="老师",
-            signature="从业3年，管理班级无数",
-            brief="从业3年，管理班级无数",
-        )
+    def add_teacher(self, options):
+        """添加授课老师的测试数据"""
+        faker = Faker(["zh_CN"])
+        for i in range(options["number"]):
+            Teacher.objects.create(
+                name=faker.unique.name(),  # 唯一的姓名
+                avatar="teacher/avatar.jpg",
+                role=random.randint(0, 2),
+                title="老师",
+                signature="从业3年，管理班级无数",
+                brief=f"从业3年，管理班级无数，联系电话：{faker.unique.phone_number()}，邮箱地址：{faker.unique.company_email()}",
+            )
+        print("添加授课老师的测试数据完成....")
+
+    def add_direction(self, options):
+        """添加学习方向的测试数据"""
+        print("添加学习方向的测试数据完成....")
+
+        """添加测试数据"""
+        # print("添加测试数据")
+        #
+        # Teacher.objects.create(
+        #     name="赵小明",
+        #     avatar="teacher/avatar.jpg",
+        #     role=1,
+        #     title="老师",
+        #     signature="从业3年，管理班级无数",
+        #     brief="从业3年，管理班级无数",
+        # )
