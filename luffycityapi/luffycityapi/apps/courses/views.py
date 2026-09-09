196ae6ba27efd644ea225dd310b82d357283f2d0
jinx <164964990+Jinxin-heart@users.noreply.github.com>
2026-09-09 14:49:54 +0800

    feature: 服务端提供课程分类列表的api接口

--- luffycityapi/luffycityapi/apps/courses/views.py
+++ luffycityapi/luffycityapi/apps/courses/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework.generics import ListAPIView
-from .models import CourseDirection
-from .serializers import CourseDirectionModelSerializer
+from .models import CourseDirection, CourseCategory
+from .serializers import CourseDirectionModelSerializer, CourseCategoryModelSerializer
 
 
 # Create your views here.
 class CourseDirectionListAPIView(ListAPIView):
     """学习方向"""
-    queryset = CourseDirection.objects.filter(is_show=True, is_deleted=False).order_by("orders","-id")
+    queryset = CourseDirection.objects.filter(is_show=True, is_deleted=False).order_by("orders", "-id")
     serializer_class = CourseDirectionModelSerializer
+    pagination_class = None
+
+
+class CourseCategoryListAPIView(ListAPIView):
+    """学习分类"""
+    queryset = CourseCategory.objects.filter(is_show=True, is_deleted=False).order_by("orders","-id")
+    serializer_class = CourseCategoryModelSerializer
+    pagination_class = None
