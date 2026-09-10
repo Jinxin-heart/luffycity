5b1961833767d8412549254d93a5295fd24b60ab
jinx <164964990+Jinxin-heart@users.noreply.github.com>
2026-09-10 12:58:50 +0800

    feature: 根据不同的学习方向与课程分类，展示课程列表信息

--- luffycityapi/luffycityapi/apps/courses/urls.py
+++ luffycityapi/luffycityapi/apps/courses/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("directions/", views.CourseDirectionListAPIView.as_view()),
-    re_path("categories/(?P<direction>\d+)/", views.CourseCategoryListAPIView.as_view()),]
+    re_path("categories/(?P<direction>\d+)/", views.CourseCategoryListAPIView.as_view()),
+    re_path(r"^(?P<direction>\d+)/(?P<category>\d+)/$", views.CourseListAPIView.as_view()),
+]
--- luffycityweb/src/api/course.js
+++ luffycityweb/src/api/course.js
@@ -14,6 +14,10 @@ const course = reactive({
     get_course_category () {
         // 获取课程分类信息
         return http.get(`/courses/categories/${this.current_direction}/`)
+    },
+    get_course_list () {
+        // 获取课程列表信息
+      return http.get(`/courses/${this.current_direction}/${this.current_category}/`)
     }
 })
 
