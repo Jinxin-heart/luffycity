335ddb966d00f4d220b43b1ff612967b0935cefc
jinx <164964990+Jinxin-heart@users.noreply.github.com>
2026-09-11 18:09:05 +0800

    feature: 集成elasticsearch到项目中提供全文搜索

--- luffycityapi/luffycityapi/apps/courses/serializers.py
+++ luffycityapi/luffycityapi/apps/courses/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import CourseDirection, CourseCategory, Course
-
+from drf_haystack.serializers import HaystackSerializer
+from .search_indexes import CourseIndex
+from django.conf import settings
 
 class CourseDirectionModelSerializer(serializers.ModelSerializer):
     """学习方向的序列化器"""
@@ -26,4 +28,15 @@ class CourseInfoModelSerializer(serializers.ModelSerializer):
             "lessons", "pub_lessons", "price", "discount"
         ]
 
+class  CourseIndexHaystackSerializer(HaystackSerializer):
+    """课程搜索的序列化器"""
+    class Meta:
+        index_classes = [CourseIndex]
+        fields = ["text", "id", "name", "course_cover", "get_level_display", "students", "get_status_display", "pub_lessons", "price", "discount", "orders"]
+
+    def to_representation(self, instance):
+        """用于指定返回数据的字段的"""
+        # 课程的图片，在这里通过elasticsearch提供的，所以不会提供图片地址左边的域名的。因此在这里手动拼接
+        instance.course_cover = f'//{settings.OSS_BUCKET_NAME}.{settings.OSS_ENDPOINT}/uploads/{instance.course_cover}'
+        return super().to_representation(instance)
 
